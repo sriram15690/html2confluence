18de8142083426d69b41900f3e02e685e7909731
Sriram Ramakrishnan <sriram15690@gmail.com>
2017-09-09 18:03:55 +0100

    Customize to run locally

--- convert.rb
+++ convert.rb
@@ -1,11 +1,29 @@
 require 'html2confluence'
 
-if ARGV.empty?
-  puts "Error: Pass HTML file as first argument"
-  exit 1
+def html_to_confluence_parser(filepath =  "./input_files")
+  input_files = Dir.glob("#{filepath}/**/*.html") - ['.', '..']
+  puts "***** Started parsing files in #{filepath} ***** \n\n"
+  input_files.each do |file|
+    puts "Parssing file #{file}"
+    output = parse(file)
+    write_output(output, file)
+  end
+  puts "\n\n***** Finished parsing & copying. Pls. check the output in #{filepath}"
 end
 
-file = File.open(ARGV[0])
-parser = HTMLToConfluenceParser.new
-parser.feed(file.read)
-puts parser.to_wiki_markup
+def parse(filepath)
+  parser = HTMLToConfluenceParser.new
+  input_file = File.open(filepath)
+  parser.feed(input_file.read)
+  output = parser.to_wiki_markup
+  output
+end
+
+def write_output output, file
+  destination_path = "./output_files/#{File.basename file}"
+  File.open(destination_path, 'w') do |f|
+    f.write output
+  end
+end
+
+html_to_confluence_parser()
